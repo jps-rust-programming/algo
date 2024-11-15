72fab329ff1cf885bd4c04f3400f0b0a1b4146e8
jpsi-0895 <jaipal.saini.0895@gmail.com>
2024-11-15 18:51:00 +0530

    rotate an array

--- src/array/rotate_array.rs
+++ src/array/rotate_array.rs
@@ -1,1 +1,9 @@
 pub fn rotate_array(nums: &mut Vec<i32>, k: usize) {}
+
+fn rotate(nums: &mut Vec<i32>, k: usize) {
+    let n = nums.len();
+    let k = k % n; // Handle k > n
+    nums.reverse();
+    nums[0..k].reverse();
+    nums[k..].reverse();
+}
